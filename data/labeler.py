fffac366355efa70ea943df5dc44517f71ff06eb
Abhilash Balaji <abhilash.balaji1998@gmail.com>
2018-10-22 14:05:44 +0530

    MLP

--- data/labeler.py
+++ data/labeler.py
@@ -5,13 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense , Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
+from keras.optimizers import SGD
 
 
 df = pd.read_csv('IRENECSV.csv')
@@ -36,12 +37,16 @@ def NBmodel(cols, pred):
 def baseline_model():
     # create model
     model = Sequential()
-    model.add(Dense(8, input_dim=5, activation='relu'))
-    model.add(Dense(8, activation='softmax'))
-    # Compile model
+    model.add(Dense(64, activation='relu', input_dim=5))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam', metrics=['accuracy'])
-    return model
+                  optimizer=sgd,
+                  metrics=['accuracy'])
 
 
 cols = ['Lat', 'Lon', 'pressure', 'wavedirection', 'waveheight']
@@ -49,7 +54,7 @@ pred = ['WindDir_L']
 le = LabelEncoder()
 le.fit(df['WindDir_L'])
 df['WindDir_L'] = le.transform(df['WindDir_L'])
-NBmodel(cols,pred)
+NBmodel(cols, pred)
 
 
 x_cols = np.array(df[cols].values)
@@ -57,10 +62,10 @@ y_cols = np.array(df[pred].values)
 y_cols = y_cols
 x_train, x_test, y_train, y_test = train_test_split(
     x_cols, y_cols, test_size=0.33)
-dummy_y=np_utils.to_categorical(y_cols)
+dummy_y = np_utils.to_categorical(y_cols)
 
 estimator = KerasClassifier(build_fn=baseline_model,
-                            epochs=20, batch_size=1, verbose=1)
+                            epochs=2, batch_size=1, verbose=1)
 kfold = KFold(n_splits=10, shuffle=True)
 
 results = cross_val_score(estimator, x_cols, dummy_y, cv=kfold)
